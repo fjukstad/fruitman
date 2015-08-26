53e1908ccd85a5d91de3880d2cef6a92e5724054
Bjørn Fjukstad <bjorn@cs.uit.no>
2015-08-26 13:13:28 +0200

    uit layout

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -8,12 +9,18 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func FileHandler(w http.ResponseWriter, r *http.Request) {
-	filename := "." + r.URL.Path
+type Scores struct {
+	Score map[string]int
+}
 
-	if r.URL.Path == "/" {
+func FileHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	filename := vars["filename"]
+
+	if filename == "" {
 		filename = "index.html"
 	}
+
 	f, err := ioutil.ReadFile(filename)
 	if err != nil {
 		http.Error(w, err.Error(), 404)
@@ -21,12 +28,28 @@ func FileHandler(w http.ResponseWriter, r *http.Request) {
 		w.Write(f)
 	}
 }
+func SubmitHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	fruit := vars["fruit"]
+
+	fmt.Println(fruit)
+
+}
+
+func GetResultsHandler(w http.ResponseWriter, r *http.Request) {
+
+}
 
 func main() {
 
 	r := mux.NewRouter()
 
 	r.HandleFunc("/", FileHandler)
+	r.HandleFunc("/{filename}", FileHandler)
+
+	r.HandleFunc("/submit/{fruit}", SubmitHandler)
+	r.HandleFunc("/getResults", GetResultsHandler)
+
 	http.Handle("/", r)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
